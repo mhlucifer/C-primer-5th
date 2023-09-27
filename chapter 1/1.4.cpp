d9b0119d0b7d4b9d1d72879801ede3b8b1f2874b
mhlucifer <47802766+mhlucifer@users.noreply.github.com>
2023-09-27 14:53:47 +1000

    Delete chapter 1 directory

--- chapter 1/1.4.cpp
+++
@@ -1,8 +0,0 @@
-# include<iostream>
-int main()
-{
-	int v1, v2, v3;
-	std::cout << "please input two numbers: " << std::endl;
-	std::cin >> v1 >> v2;
-	std::cout << "the product of " << v1 << " and " << v2 << " is " << v1 * v2 << std::endl;
-}
--- chapter 1/1.8.cpp
+++
@@ -1,2 +0,0 @@
-//correct 1£¬2 4
-//not correct 3,correct is std::cout << /* "*/" */";
